384e54c9e6ba21a5ce1d3cad9bb8721d97924ccb
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-03-16 22:32:30 -0300

    add views and modify routes accordingly

--- controller/Spotify.controller.js
+++ controller/Spotify.controller.js
@@ -3,14 +3,15 @@ const spotifyApi = require('../service/Spotify.service');
 
 class SpotifyController {
   async searchArtists(req, res) {
+
     const { artistName } = req.query;
-    console.log({ artistName })
+
     try {
       const { body: { artists: { items } } } = await spotifyApi.searchArtists(artistName)
 
       console.log('The received data from the API: ', items);
 
-      res.send(items)
+      res.render('artist-search-results', { artist: items })
     } catch (error) {
       console.log('The error while searching artists occurred: ', error);
     }
@@ -23,19 +24,21 @@ class SpotifyController {
       const { body: { items } } = await spotifyApi.getArtistAlbums(artistId)
       console.log('The received data from the API: ', items);
 
-      res.send(items);
+      res.render('albuns', { album: items });
     } catch (error) {
       console.log('The error while searching artists albuns occurred: ', error);
     }
   }
 
   async getAlbumTracks(req, res) {
-    const { trackId } = req.params;
+    const { albumId } = req.params;
     try {
-      const { body: { items } } = await spotifyApi.getAlbumTracks(trackId)
+      const { body: { items } } = await spotifyApi.getAlbumTracks(albumId)
       console.log('The received data from the API: ', items);
 
-      res.send(items);
+      res.render('tracks', { track: items })
+
+      // res.send(items);
     } catch (error) {
       console.log('The error while searching artists albuns occurred: ', error);
     }
